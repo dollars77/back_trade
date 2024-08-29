7b22b578fffa079e1c20e4fd8666b5e789c430e1
dollars77 <slot.dollars77@gmail.com>
2024-08-29 15:05:57 +0700

    first upload

---
+++ app/routes/tradelist.routes.js
@@ -0,0 +1,23 @@
+module.exports =(app)=>{
+    const tradelist = require("../controllers/tradelist.controller");
+
+    var router = require("express").Router();
+
+    router.post('/createusertrade',tradelist.createUserTrade);
+    router.get('/getoneusertrading/:id',tradelist.getOneUserTrading)
+    router.get('/getoneuseralltrade/:id',tradelist.getOneUserAllTrade)
+    router.get('/getoneuseralltradeadmin/:id',tradelist.getOneUserAllTradeAdmin)
+
+    router.get('/getuseralltradeadmin',tradelist.getUserAllTradeAdmin)
+    router.get('/getuseralltradehistoryadmin',tradelist.getUserAllTradeHistoryAdmin)
+    router.put('/adminsettrade',tradelist.AdminSetTrade)
+
+    router.post('/getOneUserTradingTimeout',tradelist.getOneUserTradingTimeout)
+
+    router.get('/callcoinapi',tradelist.callcoinapi)
+
+
+
+    app.use("/api/tradelist",router);
+    
+}
