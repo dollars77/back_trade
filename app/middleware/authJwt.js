7eeec2b4d6a3fc05a6b6a45747982c0829625384
dollars77 <slot.dollars77@gmail.com>
2024-12-20 17:58:29 +0700

    logout check

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -2,23 +2,59 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
-verifyToken = (req, res, next) => {
+// verifyToken = (req, res, next) => {
+//   let token = req.body.xaccesstoken;
+//   // let token = req.headers["x-access-token"];
+//   if (!token) {
+//     return res.status(403).send({
+//       message: "No token provided!"
+//     });
+//   }
+//   jwt.verify(token, config.secret, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({
+//         message: "Unauthorized!"
+//       });
+//     }
+//     req.userId = decoded.id;
+//     next();
+//   });
+// };
+verifyToken = async (req, res, next) => {
   let token = req.body.xaccesstoken;
-  // let token = req.headers["x-access-token"];
+
   if (!token) {
     return res.status(403).send({
       message: "No token provided!"
     });
   }
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
+
+  try {
+    const decoded = jwt.verify(token, config.secret);
+    
+    // เช็ค version กับ user ในฐานข้อมูล
+    const user = await User.findByPk(decoded.id);
+    
+    if (!user) {
       return res.status(401).send({
-        message: "Unauthorized!"
+        message: "User not found!"
       });
     }
+
+    // ถ้า version ไม่ตรงกัน แสดงว่า token หมดอายุ
+    if (user.version !== decoded.version) {
+      return res.status(401).send({
+        message: "Token is expired due to user update!"
+      });
+    }
+
     req.userId = decoded.id;
     next();
-  });
+  } catch (err) {
+    return res.status(401).send({
+      message: "Unauthorized!"
+    });
+  }
 };
 isAdmin = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
